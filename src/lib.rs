5b808b61df8284826f198388abfd26cae6804d13
OdenShirataki <git config user.email>
2022-10-09 22:43:40 +0900

    add fn create_row

--- src/lib.rs
+++ src/lib.rs
@@ -60,6 +60,7 @@ pub enum Operation<'a>{
     ,Delete{row:u32}
 }
 
+
 pub struct Data{
     data_dir:String
     ,serial: Arc<RwLock<SerialNumber>>
@@ -86,7 +87,56 @@ impl Data{
             ,fields_cache:HashMap::new()
         })
     }
-    pub fn update_row<'a>(&mut self,row:u32,activity:&Activity,term_begin:&UpdateTerm,term_end:&UpdateTerm,fields:&Vec<KeyValue<'a>>){
+
+    pub fn update(
+        &mut self
+        ,operation:&Operation
+    )->u32{
+        match operation{
+            Operation::New{
+                activity
+                ,term_begin
+                ,term_end
+                ,fields
+            }=>{
+                self.create_row(activity,term_begin,term_end,fields)
+            }
+            ,Operation::Update{
+                row
+                ,activity
+                ,term_begin
+                ,term_end
+                ,fields
+            }=>{
+                self.update_row(*row,activity,term_begin,term_end,fields);
+                *row
+            }
+            ,Operation::Delete{row}=>{
+                self.delete(*row);
+                0
+            }
+        }
+    }
+    
+    pub fn create_row(
+        &mut self
+        ,activity:&Activity
+        ,term_begin:&UpdateTerm
+        ,term_end:&UpdateTerm
+        ,fields:&Vec<KeyValue>
+    )->u32{
+        if self.serial.read().unwrap().exists_blank(){
+            let row=self.serial.write().unwrap().pop_blank().unwrap();
+            self.update_row(row,activity,term_begin,term_end,fields);
+            row
+        }else{
+            let row=self.serial.write().unwrap().add().unwrap();
+            self.update_row_with_resize(row,activity,term_begin,term_end,fields);
+            row
+        }
+    }
+
+    pub fn update_row(&mut self,row:u32,activity:&Activity,term_begin:&UpdateTerm,term_end:&UpdateTerm,fields:&Vec<KeyValue>){
         let mut handles=Vec::new();
 
         let index=self.uuid.clone();
@@ -114,41 +164,67 @@ impl Data{
             h.join().unwrap();
         }
     }
-    pub fn update(
+    fn update_row_with_resize(
         &mut self
-        ,operation:&Operation
-    )->u32{
-        match operation{
-            Operation::New{
-                activity
-                ,term_begin
-                ,term_end
-                ,fields
-            }=>{
-                if self.serial.read().unwrap().exists_blank(){
-                    let row=self.serial.write().unwrap().pop_blank().unwrap();
-                    self.update_row(row,activity,term_begin,term_end,fields);
-                    row
-                }else{
-                    self.update_new(activity,term_begin,term_end,fields)
-                }
+        ,row:u32
+        ,activity:&Activity
+        ,term_begin:&UpdateTerm
+        ,term_end:&UpdateTerm
+        ,fields:&Vec<KeyValue>
+    ){
+        let mut handles=Vec::new();
+
+        let index=self.uuid.clone();
+        handles.push(thread::spawn(move||{
+            let mut index=index.write().unwrap();
+            if let Ok(_)=index.resize_to(row){
+                index.triee_mut().update(row,Uuid::new_v4().as_u128());
             }
-            ,Operation::Update{
-                row
-                ,activity
-                ,term_begin
-                ,term_end
-                ,fields
-            }=>{
-                self.update_row(*row,activity,term_begin,term_end,fields);
-                *row
+        }));
+
+        let activity=*activity as u8;
+        let index=self.activity.clone();
+        handles.push(thread::spawn(move||{
+            let mut index=index.write().unwrap();
+            if let Ok(_)=index.resize_to(row){
+                index.triee_mut().update(row,activity);
             }
-            ,Operation::Delete{row}=>{
-                self.delete(*row);
-                0
+        }));
+
+        let term_begin=if let UpdateTerm::Overwrite(term_begin)=term_begin{
+            *term_begin
+        }else{
+            chrono::Local::now().timestamp()
+        };
+        let index=self.term_begin.clone();
+        handles.push(thread::spawn(move||{
+            let mut index=index.write().unwrap();
+            if let Ok(_)=index.resize_to(row){
+                index.triee_mut().update(row,term_begin);
             }
+        }));
+
+        let term_end=if let UpdateTerm::Overwrite(term_end)=term_end{
+            *term_end
+        }else{
+            0
+        };
+        let index=self.term_end.clone();
+        handles.push(thread::spawn(move||{
+            let mut index=index.write().unwrap();
+            if let Ok(_)=index.resize_to(row){
+                index.triee_mut().update(row,term_end);
+            }
+        }));
+
+        handles.append(&mut self.update_fields(row,fields));
+
+        for h in handles{
+            h.join().unwrap();
         }
     }
+
+    
     fn last_update_now(&mut self,row:u32){
         self.last_updated.write().unwrap().update(row,chrono::Local::now().timestamp());
     } 
@@ -270,68 +346,6 @@ impl Data{
         for h in handles{
             h.join().unwrap();
         }
-    }
-
-    fn update_new(
-        &mut self
-        ,activity:&Activity
-        ,term_begin:&UpdateTerm
-        ,term_end:&UpdateTerm
-        ,fields:&Vec<KeyValue>
-    )->u32{
-        let row=self.serial.write().unwrap().add().unwrap();
-        let mut handles=Vec::new();
-
-        let index=self.uuid.clone();
-        handles.push(thread::spawn(move||{
-            let mut index=index.write().unwrap();
-            if let Ok(_)=index.resize_to(row){
-                index.triee_mut().update(row,Uuid::new_v4().as_u128());
-            }
-        }));
-
-        let activity=*activity as u8;
-        let index=self.activity.clone();
-        handles.push(thread::spawn(move||{
-            let mut index=index.write().unwrap();
-            if let Ok(_)=index.resize_to(row){
-                index.triee_mut().update(row,activity);
-            }
-        }));
-
-        let term_begin=if let UpdateTerm::Overwrite(term_begin)=term_begin{
-            *term_begin
-        }else{
-            chrono::Local::now().timestamp()
-        };
-        let index=self.term_begin.clone();
-        handles.push(thread::spawn(move||{
-            let mut index=index.write().unwrap();
-            if let Ok(_)=index.resize_to(row){
-                index.triee_mut().update(row,term_begin);
-            }
-        }));
-
-        let term_end=if let UpdateTerm::Overwrite(term_end)=term_end{
-            *term_end
-        }else{
-            0
-        };
-        let index=self.term_end.clone();
-        handles.push(thread::spawn(move||{
-            let mut index=index.write().unwrap();
-            if let Ok(_)=index.resize_to(row){
-                index.triee_mut().update(row,term_end);
-            }
-        }));
-
-        handles.append(&mut self.update_fields(row,fields));
-
-        for h in handles{
-            h.join().unwrap();
-        }
-
-        row
     }
 
     pub fn serial(&self,row:u32)->u32{
