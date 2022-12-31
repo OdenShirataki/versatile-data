1e30188a9e5dea172ccc8c872eadb830ad86104b
Oden <oden.srtk@gmail.com>
2022-12-31 09:50:43 +0900

    impl Serialze for KeyValue

--- src/operation.rs
+++ src/operation.rs
@@ -1,6 +1,22 @@
+use serde::{ser::SerializeMap, Serialize, Serializer};
+
 pub struct KeyValue {
     pub(super) key: String,
     pub(super) value: Vec<u8>,
+}
+impl Serialize for KeyValue {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_map(Some(1))?;
+        if let Ok(s) = std::str::from_utf8(&self.value) {
+            state.serialize_entry(&self.key, s)?;
+        } else {
+            state.serialize_entry(&self.key, &self.value)?;
+        }
+        state.end()
+    }
 }
 impl KeyValue {
     pub fn new(key: impl Into<String>, value: impl Into<Vec<u8>>) -> Self {
