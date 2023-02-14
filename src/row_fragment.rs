2f58b484193c670d324f13f932a9c3f32b660227
Oden <oden.srtk@gmail.com>
2023-02-14 19:19:14 +0900

    delete blank_list and direct ref filemmap.as_ptr()

--- src/row_fragment.rs
+++ src/row_fragment.rs
@@ -1,4 +1,4 @@
-use std::{io, mem::ManuallyDrop, path::PathBuf};
+use std::{io, path::PathBuf};
 
 use file_mmap::FileMmap;
 
@@ -6,7 +6,6 @@ const U32_SIZE: usize = std::mem::size_of::<u32>();
 
 pub struct RowFragment {
     filemmap: FileMmap,
-    blank_list: ManuallyDrop<Box<u32>>,
 }
 impl RowFragment {
     pub fn new(path: PathBuf) -> io::Result<Self> {
@@ -14,37 +13,27 @@ impl RowFragment {
         if filemmap.len()? == 0 {
             filemmap.set_len(U32_SIZE as u64)?;
         }
-        let blank_list = filemmap.as_ptr() as *mut u32;
-        Ok(Self {
-            filemmap,
-            blank_list: ManuallyDrop::new(unsafe { Box::from_raw(blank_list) }),
-        })
+        Ok(Self { filemmap })
     }
     fn blank_count(&self) -> u64 {
         self.filemmap.len().unwrap() / U32_SIZE as u64 - 1
     }
-    fn remap(&mut self) {
-        self.blank_list =
-            ManuallyDrop::new(unsafe { Box::from_raw(self.filemmap.as_ptr() as *mut u32) });
-    }
     pub fn insert_blank(&mut self, row: u32) -> io::Result<u64> {
-        let r = self.filemmap.append(&row.to_ne_bytes())?;
-        self.remap();
-        Ok(r)
+        self.filemmap.append(&row.to_ne_bytes())
     }
     pub fn pop(&mut self) -> io::Result<Option<u32>> {
         let count = self.blank_count();
         Ok(if count > 0 {
-            let last = unsafe { *(&mut **self.blank_list as *mut u32).offset(count as isize) };
+            let last = unsafe { *(self.filemmap.as_ptr() as *mut u32).offset(count as isize) };
             self.filemmap.set_len(count * U32_SIZE as u64)?;
-            self.remap();
             Some(last)
         } else {
             None
         })
     }
     pub fn serial_increment(&mut self) -> u32 {
-        **self.blank_list += 1;
-        **self.blank_list
+        let blank_list = unsafe { &mut *(self.filemmap.as_ptr() as *mut u32) };
+        *blank_list += 1;
+        *blank_list
     }
 }
