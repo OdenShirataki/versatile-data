5085351eb03789f3aa55f3d58fb106379163f1b8
OdenShirataki <oden.srtk@gmail.com>
2023-09-25 20:14:05 +0900

    feat!: delete num from FieldEntity.
    rather, Field changed to IdxBinary<DataAddress>

--- src/field.rs
+++ src/field.rs
@@ -1,70 +1,13 @@
 use std::{
     fs,
-    ops::{Deref, DerefMut},
-    path::Path,
     sync::{Arc, RwLock},
 };
 
-use idx_binary::{DataAddress, DataAddressHolder, IdxBinary};
+use idx_binary::{DataAddress, IdxBinary};
 
 use crate::Data;
 
-#[derive(PartialEq, Clone, Debug)]
-pub struct FieldEntity {
-    data_address: DataAddress,
-    num: f64,
-}
-impl FieldEntity {
-    #[inline(always)]
-    pub fn data_address(&self) -> &DataAddress {
-        &self.data_address
-    }
-}
-
-impl DataAddressHolder<FieldEntity> for FieldEntity {
-    #[inline(always)]
-    fn data_address(&self) -> &DataAddress {
-        &self.data_address
-    }
-
-    #[inline(always)]
-    fn new(data_address: DataAddress, input: &[u8]) -> FieldEntity {
-        FieldEntity {
-            data_address,
-            num: unsafe { std::str::from_utf8_unchecked(input) }
-                .parse()
-                .unwrap_or(0.0),
-        }
-    }
-}
-
-pub struct Field {
-    index: IdxBinary<FieldEntity>,
-}
-
-impl Deref for Field {
-    type Target = IdxBinary<FieldEntity>;
-    fn deref(&self) -> &Self::Target {
-        &self.index
-    }
-}
-impl DerefMut for Field {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.index
-    }
-}
-
-impl Field {
-    pub fn new<P: AsRef<Path>>(path: P) -> Self {
-        Self {
-            index: IdxBinary::new(path),
-        }
-    }
-    #[inline(always)]
-    pub fn num(&self, row: u32) -> Option<f64> {
-        self.value(row).map(|v| v.num)
-    }
-}
+pub type Field = IdxBinary<DataAddress>;
 
 impl Data {
     #[inline(always)]
@@ -80,12 +23,16 @@ impl Data {
     #[inline(always)]
     pub fn field_num(&self, row: u32, name: &str) -> f64 {
         self.field(name)
-            .and_then(|v| v.read().unwrap().num(row))
+            .and_then(|v| v.read().unwrap().bytes(row))
+            .and_then(|v| unsafe { std::str::from_utf8_unchecked(v) }.parse().ok())
             .unwrap_or(0.0)
     }
 
     #[inline(always)]
-    pub(crate) fn create_field(&mut self, field_name: &str) -> &mut Arc<RwLock<Field>> {
+    pub(crate) fn create_field(
+        &mut self,
+        field_name: &str,
+    ) -> &mut Arc<RwLock<Field>> {
         let mut fields_dir = self.fields_dir.clone();
         fields_dir.push(field_name);
         fs::create_dir_all(&fields_dir).unwrap();
